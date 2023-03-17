c3a9a0e933a21bc3221ec0a81be3bb9bdaaf3579
Santiago Carral <santiagocarral@Santiagos-MacBook-Air.local>
2023-03-17 12:04:59 -0600

    Initial TecladoVirtual component with styling

--- src/components/TecladoVirtual.tsx
+++ src/components/TecladoVirtual.tsx
@@ -1,7 +1,35 @@
+import "./styles/TecladoVirtual.css";
+
 const TecladoVirtual = () => {
+  const teclas = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ"],
+    ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DEL"],
+  ];
+
   return (
     <div className="teclado-virtual">
-      <h1>Teclado Virtual</h1>
+      {teclas.map((fila, index) => {
+        return (
+          <div className="fila-teclas" key={`fila${index}`}>
+            {fila.map((letra) => {
+              if (letra.length === 1) {
+                return (
+                  <div key={letra} className="tecla">
+                    {letra}
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={letra} className="tecla large-tecla">
+                    {letra}
+                  </div>
+                );
+              }
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
