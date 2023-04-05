b50d8864c9c36ac17326b09bc2cebf1bf190807e
Santiago Carral <santiagocarral@Santiagos-MacBook-Air.local>
2023-04-05 16:44:36 -0600

    Changed DEL key icon size

--- src/components/VirtualKeyboard.tsx
+++ src/components/VirtualKeyboard.tsx
@@ -120,8 +120,8 @@ const VirtualKeyboard: FC<VirtualKeyboardProps> = ({
                 return (
                   <LargeKey key={key} onClick={handleDelete}>
                     <svg
-                      width="11"
-                      height="8"
+                      width="31"
+                      height="18"
                       viewBox="0 0 23 17"
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
